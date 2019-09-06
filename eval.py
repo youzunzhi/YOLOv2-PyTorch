b78d3d3a000ca13dce866149d28ebfc460c32898
youzunzhi <youzunzhi@163.com>
2019-09-07 00:29:01 +0800

    remote debug

--- eval.py
+++ eval.py
@@ -11,7 +11,7 @@ def main():
                         nargs=argparse.REMAINDER)
     args = parser.parse_args()
     import pydevd_pycharm
-    pydevd_pycharm.settrace('169.254.253.52', port=12345, stdoutToServer=True, stderrToServer=True)
+    pydevd_pycharm.settrace('140.113.138.32', port=12345, stdoutToServer=True, stderrToServer=True)
     if args.config_file != "":
         cfg.merge_from_file(args.config_file)
     cfg.merge_from_list(args.opts)
