7bea16b65f4a69b0c5b519f7a1c2c8f9e71d606a
youzunzhi <youzunzhi@163.com>
2020-03-11 12:47:06 +0800

    fix typo

--- config/train_cfg.py
+++ config/train_cfg.py
@@ -6,7 +6,7 @@ _C.OUTPUT_DIR = 'runs/'
 _C.EXPERIMENT_NAME = 'coco_pretrained'
 _C.CUDA_ID = '0'
 _C.EVAL_INTERVAL = 10
-_C.SAVE_INTERNAL = 10
+_C.SAVE_INTERVAL = 10
 
 train_on = 'coco'
 if train_on == 'coco':
