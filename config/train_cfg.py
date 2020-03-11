a4007208409d3e4bd6ec6add1843e81898a3cac3
youzunzhi <youzunzhi@163.com>
2020-03-11 11:52:00 +0800

    try coco truncated

--- config/train_cfg.py
+++ config/train_cfg.py
@@ -3,19 +3,19 @@ import torch
 
 _C = CN()
 _C.OUTPUT_DIR = 'runs/'
-_C.EXPERIMENT_NAME = 'tiny_voc_trained'
+_C.EXPERIMENT_NAME = 'coco_pretrained'
 _C.CUDA_ID = '0'
 _C.EVAL_INTERVAL = 10
 _C.SAVE_INTERNAL = 10
 
-train_on = 'voc'
+train_on = 'coco'
 if train_on == 'coco':
     _C.MODEL_CFG_FNAME = "pjreddie_files/yolov2.cfg"
 elif train_on == 'voc':
     # _C.MODEL_CFG_FNAME = "pjreddie_files/yolov2-voc.cfg"
     _C.MODEL_CFG_FNAME = "pjreddie_files/yolov2-tiny-voc.cfg"
-# _C.WEIGHTS_FNAME = "weights/darknet19_448.conv.23"
-_C.WEIGHTS_FNAME = "weights/yolov2-tiny-voc.weights"
+_C.WEIGHTS_FNAME = "weights/darknet19_448.conv.23"
+# _C.WEIGHTS_FNAME = "weights/yolov2-tiny-voc.weights"
 # _C.WEIGHTS_FNAME = "weights/yolov2.weights"
 _C.CONF_THRESH = 0.005
 _C.NMS_THRESH = 0.45
