a843b8c04121e769ec185cca74541ba63162c22b
youzunzhi <youzunzhi@163.com>
2020-03-12 21:05:05 +0800

    eval fix bug try again

--- utils/computation.py
+++ utils/computation.py
@@ -140,6 +140,7 @@ def get_batch_metrics(predictions, batch_targets):
                     if target_i not in target_detected and iou_targets_pred[target_i] >= iou_thresh and pred_label == targets_labels[target_i]:
                         true_positives[pred_i] = 1
                         target_detected.append(target_i)
+                        break
 
                 # for target_i, target_box in enumerate(target_boxes):
                 #     if target_i not in target_detected and iou_targets_pred[target_i] >= iou_thresh and pred_label == targets_labels[target_i]:
