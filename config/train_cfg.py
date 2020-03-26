d14ff79dd43b30a8a9493fe93fe83f0551e7c1e6
youzunzhi <youzunzhi@163.com>
2020-03-26 23:28:52 +0800

    coco_pretrain try again

--- config/train_cfg.py
+++ config/train_cfg.py
@@ -3,12 +3,12 @@ import torch
 
 _C = CN()
 _C.OUTPUT_DIR = 'runs/'
-_C.EXPERIMENT_NAME = 'voc_pretrain'
+_C.EXPERIMENT_NAME = 'coco_pretrain'
 _C.CUDA_ID = '0'
 _C.EVAL_INTERVAL = 10
 _C.SAVE_INTERVAL = 'best'
 
-train_on = 'voc'
+train_on = 'coco'
 if train_on == 'coco':
     _C.MODEL_CFG_FNAME = "pjreddie_files/yolov2.cfg"
 elif train_on == 'voc':
